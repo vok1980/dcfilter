164fdec4a072e13418a667d5234a5ed7404bae78
Vyacheslav Koscheev <v.koscheev@2gis.ru>
2023-11-09 02:03:23 +0700

    Filter cycled nodes by dependencies

--- src/dcfilter.c
+++ src/dcfilter.c
@@ -1,9 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <assert.h>
 
 #include <graphviz/cgraph.h>
 
 #include <getopt.h>
+
+#include "bit_array/bit_array.h"
+
 
 static FILE *inFile;
 static FILE *outFile;
@@ -99,10 +103,19 @@ static void init(int argc, char *argv[])
 }
 
 
+struct DependenciesTable
+{
+    BIT_ARRAY *deps;
+    Agnode_t *node;
+};
+typedef struct DependenciesTable DependenciesTable;
+
+
 int main(int argc, char *argv[])
 {
     Agraph_t *g;
     Agnode_t *n;
+    Agedge_t *e;
 
     init(argc, argv);
 
@@ -116,10 +129,116 @@ int main(int argc, char *argv[])
         return -1;
     }
 
+    int nodes_count = agnnodes(g);
+
+    if (Verbose)
+        fprintf(stderr, "Found %d nodes\n", nodes_count);
+
+    DependenciesTable *table = calloc(nodes_count, sizeof(DependenciesTable));
+
+    int idx = 0;
+    // fill nodes in table
+    for (n = agfstnode(g); n; n = agnxtnode(g, n), ++idx)
+    {
+        assert(idx < nodes_count);
+        table[idx].node = n;
+        table[idx].deps = NULL;
+    }
+
+    assert(idx == nodes_count);
+    BIT_ARRAY *modified = bit_array_create(nodes_count);
+
+    // fill dependencies if any exists for node
+    for (int i = 0; i < nodes_count; ++i)
+    {
+        assert(NULL == table[i].deps);
+
+        for (e = agfstin(g, table[i].node); e; e = agnxtin(g, e))
+        {
+            if (NULL == table[i].deps)
+            {
+                table[i].deps = bit_array_create(nodes_count);
+            }
+
+            if (NULL != table[i].deps)
+            {
+                for (int j = 0; j < nodes_count; ++j)
+                {
+                    if (table[j].node == e->node)
+                    {
+                        bit_array_set(table[i].deps, j);
+                        bit_array_set(modified, i);
+
+                        if (table[j].deps)
+                        {
+                            bit_array_or(table[i].deps, table[i].deps, table[j].deps);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    BIT_ARRAY *zero = bit_array_create(nodes_count);
+
+    // Add transitive dependencies
+    while(!bit_array_eq(modified, zero))
+    {
+        for (int i = 0; i < nodes_count; ++i)
+        {
+            if (bit_array_get(modified, i))
+            {
+                for (int j = 0; j < nodes_count; ++j)
+                {
+                    if (i != j && table[j].deps && table[j].deps)
+                    {
+                        if (bit_array_get(table[j].deps, i))
+                        {
+                            BIT_ARRAY *or_res = bit_array_create(nodes_count);
+                            bit_array_or(or_res, table[i].deps, table[j].deps);
+
+                            if (!bit_array_eq(or_res, table[j].deps))
+                            {
+                                bit_array_set(modified, j);
+                            }
+
+                            bit_array_free(table[j].deps);
+                            table[j].deps = or_res;
+                        }
+                    }
+                }
+
+                bit_array_unset(modified, i);
+            }
+        }
+    }
+
+    // If node does not depend on itself, than it does not enters any cycle.
+    for (int i = 0; i < nodes_count; ++i)
+    {
+        if (NULL != table[i].deps)
+        {
+            if (!bit_array_get(table[i].deps, i))
+            {
+                agdelnode(g, table[i].node);
+            }
+        }
+    }
+
     if (doWrite) {
         agwrite(g, outFile);
         fflush(outFile);
     }
 
+    for (int i = 0; i < nodes_count; ++i)
+    {
+        if (NULL != table[i].deps)
+        {
+            bit_array_free(table[i].deps);
+        }
+    }
+
+    bit_array_free(modified);
+    free(table);
     return 0;
 }
