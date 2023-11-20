484643963709833753f1e47c2a0c4b2d063ccdb8
Vyacheslav Koscheev <v.koscheev@2gis.ru>
2023-11-20 10:51:53 +0700

    Dependency from conan

---
+++ build-gcc-12.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+rm -rf build && mkdir build && cd build \
+    && conan install .. --build=missing -s compiler=gcc -s compiler.version=12 -s compiler.libcxx=libstdc++11 --generator=cmake_paths -s build_type=Release \
+    && cmake .. \
+        -G Ninja \
+        -DCMAKE_TOOLCHAIN_FILE=conan_paths.cmake \
+        -DCMAKE_INSTALL_PREFIX=../install \
+        -DCMAKE_C_COMPILER=gcc-12 \
+        -DCMAKE_CXX_COMPILER=g++-12 \
+        -DCMAKE_BUILD_TYPE=Release \
+    && cmake --build . \
+    && ctest \
+    && cmake --build . --target=install
