da0f2831691d1ef18f3033bb28042fd5018e1dfd
Vyacheslav Koscheev <v.koscheev@2gis.ru>
2023-12-09 00:44:31 +0700

    Change conan generator in default build script

--- build-gcc-12.sh
+++ build-gcc-12.sh
@@ -6,11 +6,11 @@ rm -rf build && mkdir build && cd build \
         -s compiler=gcc \
         -s compiler.version=12 \
         -s compiler.libcxx=libstdc++11 \
-        --generator=cmake_paths \
+        --generator=CMakeToolchain \
         -s build_type=Release \
     && cmake .. \
         -G Ninja \
-        -DCMAKE_TOOLCHAIN_FILE=conan_paths.cmake \
+        -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake \
         -DCMAKE_INSTALL_PREFIX=../install \
         -DCMAKE_C_COMPILER=gcc-12 \
         -DCMAKE_CXX_COMPILER=g++-12 \
